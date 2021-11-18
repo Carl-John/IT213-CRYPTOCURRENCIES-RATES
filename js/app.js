0d2b620e90c9887ef9d05eb161784a44ca69d40d
Carl-John <jy081320@gmail.com>
2021-11-18 10:58:31 +0800

    Reading Values from the Form

--- js/app.js
+++ js/app.js
@@ -2,3 +2,29 @@
 
 const cryptoAPI = new CryptoAPI();
 const ui = new UI();
+
+//Create the Variables
+
+const form = document.getElementById('form');
+
+
+
+//Add EventListener
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //Read Currencies
+    const currencySelect = document.getElementById('currency').value;
+    //Read Cryptocurrencies
+    const cryptoCurrencySelect = document.getElementById(cryptocurrency).value;
+
+
+    //Validate that the select has something
+    if(currencySelect === '' || cryptoCurrencySelect === '') {
+        //Display an error
+        console.log('error');
+    } else {
+        console.log('succes');
+    } 
+    
+})
