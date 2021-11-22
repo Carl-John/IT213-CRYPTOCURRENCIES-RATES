3c886e6f30b1d669b986d1e756f2ca173f0d0dab
Carl-John <jy081320@gmail.com>
2021-11-22 14:49:27 +0800

    Finishing the Project

--- js/ui.js
+++ js/ui.js
@@ -47,25 +47,53 @@ class UI {
     //Prints the result of the valuation/rate
     displayResult(result, currency) {
         
+        console.log(result);
+
         //Read the currency
         let currencyName;
         currencyName = 'price_' + currency.toLowerCase();
         //Read the result from the object
         const value = result[currencyName];
 
-        let HTMLTemplate = '';
+        //Remove the previous result
+        const prevResult = document.querySelector('#result > div');
+        if(prevResult) {
+            prevResult.remove();
+        }
 
+        let HTMLTemplate = '';
         HTMLTemplate += `
             <div class="card cyan draken-3">
                 <div class"card-content white-text">
                     <span class="card-title">Result</span>
                     <p>The Price of ${result.name} from ${currency} is $ ${value}</p>
-
+                    <p>Last Hour: ${result.percentage_change_1h} </p>
+                    <p>Last Day: ${result.percentage_change_124h} </p>
+                    <p>Last 7 Days: ${result.percentage_change_7d} </p>
                 </div>
             </div>
         `;
 
+        //Print the Spinner
+        this.showSpinner();
+
+        //After 3sec print the result and remove the spinner
+       setTimeout(() => {
+           //Print the result
         const divResult = document.querySelector('#result');
         divResult.innerHTML = HTMLTemplate;
+
+        //Hide the spinner
+        document.querySelector('.spinner img').remove();
+       }, 3000);
+
+        
+    }
+
+    //Prints the Spinner
+    showSpinner() {
+        const spinnerGIF = document.createElement('img');
+        spinnerGIF.src = 'img/spinner.gif';
+        document.querySelector('.spinner').appendChild(spinnerGIF);
     }
 }
